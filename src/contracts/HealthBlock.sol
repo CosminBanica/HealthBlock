4578087ed33711bc3b26a7df3fd3b3a03dfcb314
CosminBanica <cosmin.banica@yahoo.com>
2022-04-30 22:18:43 +0300

    records to mapping and added shareRecords

--- src/contracts/HealthBlock.sol
+++ src/contracts/HealthBlock.sol
@@ -1,11 +1,17 @@
 pragma solidity >=0.4.21 <0.6.0;
-
-import "./Record.sol";
+pragma experimental ABIEncoderV2;
 
 contract HealthBlock {
     struct Institution {
         address institutionAddress;
         string name;
+        string link;
+    }
+
+    struct Record {
+        address institution;
+        address doctor;
+        string timestamp;
         string link;
     }
 
@@ -17,10 +23,8 @@ contract HealthBlock {
     mapping(address => bool) public isInstitution;
     mapping(address => bool) public isPatient;
     mapping(address => bool) public isDoctor;
-
-    constructor() public {
-
-    }
+    mapping(address => address[]) public accessList;
+    mapping(address => Record[]) public records;
 
     // Register patient
     function registerPatient() public {
@@ -32,11 +36,11 @@ contract HealthBlock {
     }
 
     // Register institution
-    function registerInstitution(string memory _name, string memory _link) public {
+    function registerInstitution(string memory _name, string memory link) public {
         // Add user to institutions array if not a patient/doctor or existing institution
         if (!isInstitution[msg.sender] && !isPatient[msg.sender] && !isDoctor[msg.sender]) {
             isInstitution[msg.sender] = true;
-            institutions.push(Institution({institutionAddress: msg.sender, name: _name, link: _link}));
+            institutions.push(Institution({institutionAddress: msg.sender, name: _name, link: link}));
         }
     }
 
@@ -48,5 +52,53 @@ contract HealthBlock {
             doctors.push(msg.sender);
         }
     }
+
+    // Add medical record
+    function addRecord(address patient, address doctor, string memory timestamp, string memory link) public {
+        // Only institutions can call this function
+        require(isInstitution[msg.sender], "caller must be institution");
+
+        // Patient and doctor must be valid
+        require(isPatient[patient], "patient must be valid");
+        require(isDoctor[doctor], "doctor must be valid");
+
+        // Should also check timestamp and link
+
+        // Add record to records array if patient and doctor are valid
+        records[patient].push(Record({institution: msg.sender, doctor: doctor, timestamp: timestamp, link: link}));
+    }
+
+    // MUST CHECK NOT TO ADD TWICE
+    // Share patient records with institution/doctor
+    function shareRecords(address entity) public {
+        // Only patients can call this function
+        require(isPatient[msg.sender], "caller must be patient");
+
+        // Can only share record with institution/doctor
+        require(isInstitution[entity] || isDoctor[entity], "can only share with institution/doctor");
+
+        // Add entity to user access list
+        accessList[msg.sender].push(entity);
+    }
+
+    // Return patient records if caller has access
+    function getRecords(address patient) public view returns(Record[] memory){
+        // Only institution and doctors can call this function
+        require(isInstitution[msg.sender] || isDoctor[msg.sender], "caller must be institution/doctor");
+
+        // Get patient access list
+        address[] memory patientAccList = accessList[patient];
+
+        // Caller must be in patient access list
+        for (uint i = 0; i < patientAccList.length; i++) {
+            if (patientAccList[i] == msg.sender) {
+                return records[patient];
+            }
+        }
+
+        require(false, "caller is not in patient access list");
+    }
+
+    // Return all patient records to which caller has access
 
 }
--- test/HealthBlock.test.js
+++ test/HealthBlock.test.js
@@ -64,5 +64,44 @@ contract('HealthBlock', ([owner, patient, institution, doctor]) => {
             result = await healthBlock.isDoctor(doctor)
             assert.equal(result.toString(), 'true', 'doctor is registered after registering')
         })
+
+        it('adds record', async () => {
+            let result
+
+            // Register patient, institution and doctor
+            await healthBlock.registerPatient({ from: patient })
+            await healthBlock.registerInstitution('FakeInstitution', 'fakelink.com', { from: institution })
+            await healthBlock.registerDoctor({ from: doctor })
+
+            // Add record
+            await healthBlock.addRecord(patient, doctor, '11/21/11/12:00:01', 'record.link.com', { from: institution })
+
+            // Check record is added properly
+            result = await healthBlock.records(patient, 0)
+            assert.equal(result.institution, institution, 'institution in record is correct')
+            assert.equal(result.doctor, doctor, 'doctor in record is correct')
+            assert.equal(result.timestamp, '11/21/11/12:00:01', 'institution in record is correct')
+            assert.equal(result.link, 'record.link.com', 'institution in record is correct')
+        })
+
+        it ('adds institution and doctor to access list', async () => {
+            let result
+
+            // Register patient, institution and doctor
+            await healthBlock.registerPatient({ from: patient })
+            await healthBlock.registerInstitution('FakeInstitution', 'fakelink.com', { from: institution })
+            await healthBlock.registerDoctor({ from: doctor })
+
+            // Add institution and doctor to access list
+            await healthBlock.shareRecords(institution, { from: patient })
+            await healthBlock.shareRecords(doctor, { from: patient })
+
+            // Check that institution and doctor are in patient access list
+            result = await healthBlock.accessList(patient, 0)
+            assert.equal(result, institution, 'institution is in access list')
+
+            result = await healthBlock.accessList(patient, 1)
+            assert.equal(result, doctor, 'doctor is in access list')
+        })
     })
 })
